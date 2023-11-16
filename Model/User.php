481e028ecd4cafd030152d6fd825fbd29fab7a31
Marko Shemechko <mshemechko@n-ix.com>
2023-11-16 09:45:18 +0200

    Add groups to User

--- Model/User.php
+++ Model/User.php
@@ -20,7 +20,7 @@ use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
  * @author Thibault Duplessis <thibault.duplessis@gmail.com>
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
-abstract class User implements UserInterface, EquatableInterface, \Serializable
+abstract class User implements UserInterface, GroupableInterface, EquatableInterface, \Serializable
 {
     /**
      * @var mixed
@@ -89,6 +89,11 @@ abstract class User implements UserInterface, EquatableInterface, \Serializable
      * @var \DateTime|null
      */
     protected $passwordRequestedAt;
+
+    /**
+     * @var GroupInterface[]|Collection
+     */
+    protected $groups;
 
     /**
      * @var array
@@ -396,6 +401,60 @@ abstract class User implements UserInterface, EquatableInterface, \Serializable
 
         foreach ($roles as $role) {
             $this->addRole($role);
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGroups()
+    {
+        return $this->groups ?: $this->groups = new ArrayCollection();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGroupNames()
+    {
+        $names = [];
+        foreach ($this->getGroups() as $group) {
+            $names[] = $group->getName();
+        }
+
+        return $names;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasGroup($name)
+    {
+        return in_array($name, $this->getGroupNames());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addGroup(GroupInterface $group)
+    {
+        if (!$this->getGroups()->contains($group)) {
+            $this->getGroups()->add($group);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeGroup(GroupInterface $group)
+    {
+        if ($this->getGroups()->contains($group)) {
+            $this->getGroups()->removeElement($group);
         }
 
         return $this;
